b099894d2cd43f3e5b05b2b725b736473d6a33d4
David Miller <david.lauchlin.miller@gmail.com>
2025-03-17 16:45:32 -0400

    classify platanus script setup

---
+++ classify_platanus.R
@@ -0,0 +1,5 @@
+# Make a script that loads in a subset of the tree database for a neighborhood with likely a lot of Platanus unmapped
+# Use existing extracted point phenology data + lidar polygon variables (and temperature?? perhaps not needed) to train random forest
+# Ranger (random forest) training for binary classifier of Platanus / not Platanus (very simple)
+# Apply back onto unmapped trees in neighborhood for binary classifier demonstration
+
